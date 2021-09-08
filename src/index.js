24ec785895cbc0bbb73a89a8b30c967905efa716
Larry Shields <larry.shields@gmail.com>
2021-09-08 20:46:15 +0000

    Added Team Checks

--- src/index.js
+++ src/index.js
@@ -43,49 +43,54 @@ client.get(url).then(({ data }) => {
 
     let columns = $(ele).find('td');
     let away_team_section = $(columns.get(0)).html();
-    let away_image_url = htmlhelper.getAttributeValue(away_team_section, 'img', 'src');
-    game.awayteam = await drivehelper.getTeamAbbreviation(away_image_url);
-
     let home_team_section = $(columns.get(1)).html();
-    let home_image_url = htmlhelper.getAttributeValue(home_team_section, 'img', 'src');
-    game.hometeam = await drivehelper.getTeamAbbreviation(home_image_url);
-
-    let gameid_section = $(columns.get(2)).html();
-    let gameid_url = htmlhelper.getAttributeValue(gameid_section, 'a', 'href');
-    let parts = gameid_url.split('/');
-    let length = parts.length;
-    if (length > 0) {
-      game.gameid = parts[length - 1];
-    }
-    if (scheduleOnly === 0) {
-      console.log('Pulling Stats for ' + game.gameid);
-      // Get the Stats
-      boxscore_url = 'https://www.espn.com/nfl/boxscore/_/gameId/' + game.gameid;
-      try {
-        let boxscore = await stathelper.getGameStats(boxscore_url);
-        game.boxscore = boxscore;
-
-        // Get matchup
-        matchup_url = 'https://www.espn.com/nfl/matchup?gameId=' + game.gameid;
-        let matchup = await teamhelper.getTeamStats(matchup_url);
-        game.matchup = matchup;
-
-        // Get Big Plays
-        drive_url = 'https://www.espn.com/nfl/playbyplay/_/gameId/' + game.gameid;
-        let bigplays = await drivehelper.getDriveStats(drive_url);
-        game.bigplays = bigplays;
-
-      } catch (error) {
-        console.log(error);
+    
+    if (home_team_section && away_team_section) {
+      let home_image_url = htmlhelper.getAttributeValue(home_team_section, 'img', 'src');
+      let away_image_url = htmlhelper.getAttributeValue(away_team_section, 'img', 'src');
+      game.awayteam = await drivehelper.getTeamAbbreviation(away_image_url);
+      game.hometeam = await drivehelper.getTeamAbbreviation(home_image_url);
+      let gameid_section = $(columns.get(2)).html();
+      let gameid_url = htmlhelper.getAttributeValue(gameid_section, 'a', 'href');
+      let parts = gameid_url.split('/');
+      let length = parts.length;
+      if (length > 0) {
+        game.gameid = parts[length - 1];
       }
+      if (scheduleOnly === 0) {
+        console.log('Pulling Stats for ' + game.gameid);
+        // Get the Stats
+        boxscore_url = 'https://www.espn.com/nfl/boxscore/_/gameId/' + game.gameid;
+        try {
+          let boxscore = await stathelper.getGameStats(boxscore_url);
+          game.boxscore = boxscore;
+
+          // Get matchup
+          matchup_url = 'https://www.espn.com/nfl/matchup?gameId=' + game.gameid;
+          let matchup = await teamhelper.getTeamStats(matchup_url);
+          game.matchup = matchup;
+
+          // Get Big Plays
+          drive_url = 'https://www.espn.com/nfl/playbyplay/_/gameId/' + game.gameid;
+          let bigplays = await drivehelper.getDriveStats(drive_url);
+          game.bigplays = bigplays;
+
+        } catch (error) {
+
+          console.log("Error Pulling Game Id: " + game.gameid);
+          console.log(error);
+        }
+      } else {
+        console.log('Outputing schedule for ' + game.gameid);
+      }
+      const fs = require('fs');
+      // Output the game
+      console.log('OUTPUTING GAME ' + game.gameid);
+      fs.writeFileSync(outputDirectory + game.gameid + '.json', JSON.stringify(game));
+      return game;
     } else {
-      console.log('Outputing schedule for ' + game.gameid);
+      console.log("NO TEAMS FOUND.");
     }
-    const fs = require('fs');
-    // Output the game
-    console.log('OUTPUTING GAME ' + game.gameid);
-    fs.writeFileSync(outputDirectory + game.gameid + '.json', JSON.stringify(game));
-    return game;
   }).get();
 
   Promise.all(games);
