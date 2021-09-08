ebc91685d12e6c674c62a8e15dedd51fb40636c1
Larry Shields <larry.shields@gmail.com>
2021-09-08 20:46:32 +0000

    Added Pull Script

--- src/pull_data.sh
+++ src/pull_data.sh
@@ -1,10 +1,10 @@
 input_week=1
-input_year=2019
+input_year=2020
 input_type=2
 
-while [ $input_week -lt 2 ]; do
+while [ $input_week -lt 18 ]; do
   echo "Pulling games for $input_year week $input_week"
   command="node index.js $input_week $input_year $input_type 0"
   eval $command
-  input_week=[$input_week+1]
+  ((input_week+=1))
 done
