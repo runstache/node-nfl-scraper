0152f08b2bcb7c8290f8b96c7904bc33acc341c0
Larry Shields <larry.shields@gmail.com>
2021-09-09 05:40:43 +0000

    Pull different year

--- src/pull_data.sh
+++ src/pull_data.sh
@@ -1,10 +1,10 @@
 input_week=1
-input_year=2020
+input_year=2021
 input_type=2
 
 while [ $input_week -lt 18 ]; do
   echo "Pulling games for $input_year week $input_week"
-  command="node index.js $input_week $input_year $input_type 0"
+  command="node index.js $input_week $input_year $input_type 1"
   eval $command
   ((input_week+=1))
 done
